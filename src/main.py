4d9571df6f174197f316d4da3be0c84df0550fc8
Jesse <jesusgarza2511@gmail.com>
2025-06-10 09:10:25 -0500

    Import SimpleImputer class from Scikit-Learn (sklearn) to replace missing values and then impute missing data with the mean

--- src/main.py
+++ src/main.py
@@ -5,9 +5,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
+# create the matrix of features (X)
+# create the dependent variable vector (y)
 dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-print(x)
+print(X)
 print(y)
+print()
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
